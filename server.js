0157f7cf726b4fa0a4a974217e6f04734e6d687e
ronak suba <shrutiinfoaios@gmail.com>
2025-11-01 15:21:53 +0530

    enabled cors origin

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ const bodyParser = require('body-parser');
 const jsonwebtoken = require("jsonwebtoken");
 const jwtSecretToken = process.env.JWT_SECRET_ACCESS_TOKEN;
 const usersRoute = require('./src/routes/usersRoute');
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
