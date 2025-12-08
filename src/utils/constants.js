408cc7abe572e43d72c6055c97f7c1f2bbd2f80a
ronak suba <shrutiinfoaios@gmail.com>
2025-12-08 16:40:05 +0530

    forgot password feature

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
-//export const PORT = 3000
-export const HTTP_400 = 404
-export const HTTP_500 = 404
-export const SOMETHING_WENT_WRONG = "Something went wrong"
-export const RETRY_DELAY_MS = 2000 // Initial retry delay (increases exponentially)
-export const DB_MAX_RETRY = 5 // Maximum reconnection attempts
+const PORT = 3000
+const HTTP_400 = 404
+const HTTP_500 = 404
+const SOMETHING_WENT_WRONG = "Something went wrong"
+const RETRY_DELAY_MS = 2000 // Initial retry delay (increases exponentially)
+const DB_MAX_RETRY = 5 // Maximum reconnection attempts
